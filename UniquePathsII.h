4a5bf1388df42844e455335470f49669a2d2af38
AnnieKim <anniekim.pku@gmail.com>
2013-04-09 09:35:27 +0800

    Refactor some expression.

--- UniquePathsII.h
+++ UniquePathsII.h
@@ -27,12 +27,12 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         int dp[m][n];
-
-        dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+        if (obstacleGrid[0][0] == 1) return 0;
+        dp[0][0] = 1;
         for (int i = 1; i < m; i++)
-            dp[i][0] = obstacleGrid[i][0] == 1 || dp[i-1][0] == 0 ? 0 : 1;
+            dp[i][0] = obstacleGrid[i][0] == 1? 0 : dp[i-1][0];
         for (int j = 1; j < n; j++)
-            dp[0][j] = obstacleGrid[0][j] == 1 || dp[0][j-1] == 0 ? 0 : 1;
+            dp[0][j] = obstacleGrid[0][j] == 1? 0 : dp[0][j-1];
         
         for (int i = 1; i < m; i++)
             for (int j = 1; j < n; j++)
