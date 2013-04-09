aa480ecf1d032813e25d6e5ca30ef3d9ee6989a0
AnnieKim <anniekim.pku@gmail.com>
2013-04-09 09:04:59 +0800

    Unique Paths (version 2: Dynamic Programming version).

---
+++ UniquePaths.h
@@ -0,0 +1,48 @@
+/*
+ Author:     Annie Kim, anniekim.pku@gmail.com
+ Date:       Apr 8, 2013
+ Update:     Apr 9, 2013
+ Problem:    Unique Paths
+ Difficulty: Easy
+ Source:     http://leetcode.com/onlinejudge#question_62
+ Notes:
+ A robot is located at the top-left corner of a m x n grid.
+ The robot can only move either down or right at any point in time. The robot is trying to reach 
+ the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ How many possible unique paths are there?
+
+ Solution: 
+ 1. Use formula C(x,t) = t!/(x!*(t-x)!) (x should be large for calculation).
+ 2. Dynamic programming. UP(i,j) = UP(i-1,j) + UP(i,j-1).
+ */
+
+class Solution {
+public:
+    int uniquePaths_1(int m, int n) {
+        if (m == 1  || n == 1) return 1;
+        int t = (m-1)+(n-1);
+        int x = (m > n) ? (m-1) : (n-1);
+        
+        long long res = 1;
+        for (int i = t; i > x; i--)
+            res *= i;
+        for (int i = t-x; i > 1; i--)
+            res /= i;
+        
+        return res;
+    }
+
+    int uniquePaths_2(int m, int n) {
+        int dp[m][n];
+        for (int i = 0; i < m; i++)
+            dp[i][0] = 1;
+        for (int j = 0; j < n; j++)
+            dp[0][j] = 1;
+        
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        
+        return dp[m-1][n-1];
+    }
+};
