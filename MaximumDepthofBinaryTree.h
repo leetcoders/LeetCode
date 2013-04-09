040c6a8e82ecbc31fa398109303e5e41bf06fb11
Annie <anniekim.pku@gmail.com>
2013-04-09 10:29:34 +0800

    Refactor. Leaf node can also use last sentence.

--- MaximumDepthofBinaryTree.h
+++ MaximumDepthofBinaryTree.h
@@ -23,11 +23,7 @@
 class Solution {
 public:
     int maxDepth(TreeNode *root) {
-        if (!root) 
-            return 0;
-        if (!root->left && !root->right) 
-            return 1;
-        
+        if (!root) return 0;
         return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
