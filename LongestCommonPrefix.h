9bff127c5ba76188fc6d41dc56d42ef41d496fc8
AnnieKim <anniekim.pku@gmail.com>
2013-04-16 22:12:29 +0800

    Longest Common Prefix.

---
+++ LongestCommonPrefix.h
@@ -0,0 +1,28 @@
+/*
+ Author:     Annie Kim, anniekim.pku@gmail.com
+ Date:       Apr 16, 2013
+ Problem:    Longest Common Prefix
+ Difficulty: Easy
+ Source:     http://leetcode.com/onlinejudge#question_14
+ Notes:
+ Write a function to find the longest common prefix string amongst an array of strings.
+
+ Solution: ...
+ */
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string> &strs) {
+        if (strs.size() == 0) return string();
+        string res;
+        for (int i = 0; i < strs[0].size(); ++i)
+        {
+            char ch = strs[0][i];
+            for (int j = 1; j < strs.size(); ++j)
+                if (i == strs[j].size() || strs[j][i] != ch)
+                    return res;
+            res.push_back(ch);
+        }
+        return res;
+    }
+};
