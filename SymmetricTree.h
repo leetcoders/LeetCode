7881c4b02654e41c9c3c715000d2492e85f20dff
AnnieKim <anniekim.pku@gmail.com>
2013-04-22 15:44:31 +0800

    Symmetric Tree.

---
+++ SymmetricTree.h
@@ -0,0 +1,75 @@
+/*
+ Author:     Annie Kim, anniekim.pku@gmail.com
+ Date:       Apr 22, 2013
+ Problem:    Symmetric Tree
+ Difficulty: Easy
+ Source:     http://leetcode.com/onlinejudge#question_101
+ Notes:
+ Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ For example, this binary tree is symmetric:
+     1
+    / \
+   2   2
+  / \ / \
+ 3  4 4  3
+ But the following is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ Note:
+ Bonus points if you could solve it both recursively and iteratively.
+
+ Solution: 1. Recursive solution 2.Iterative way (queue).
+ */
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric_1(TreeNode *root) {
+        if (!root) return true;
+        return isSymmetricRe(root->left, root->right);
+    }
+
+    bool isSymmetricRe(TreeNode *t1, TreeNode *t2)
+    {
+        if (!t1 && !t2) return true;
+        if (!t1 || !t2 || t1->val != t2->val) return false;
+        if (!isSymmetricRe(t1->left, t2->right) ||
+            !isSymmetricRe(t1->right, t2->left)) 
+            return false;
+        return true;
+    }
+
+    bool isSymmetric_2(TreeNode *root) {
+        if (!root) return true;
+        queue<TreeNode *> q;
+        q.push(root->left);
+        q.push(root->right);
+        while (!q.empty())
+        {
+            TreeNode *t1 = q.front(); q.pop();
+            TreeNode *t2 = q.front(); q.pop();
+            if (!t1 && !t2)
+                continue;
+            else if (!t1 || !t2 || t1->val != t2->val) 
+                return false;
+            else
+            {
+                q.push(t1->left);
+                q.push(t2->right);
+                q.push(t1->right);
+                q.push(t2->left);
+            }
+        }
+        return true;
+    }
+};
