079d2dd48d3becdeaee0774c0a3ba2c8b60b17a7
AnnieKim <anniekim.pku@gmail.com>
2013-04-28 19:36:29 +0800

    Refactor.

--- BestTimetoBuyandSellStockII.h
+++ BestTimetoBuyandSellStockII.h
@@ -38,13 +38,11 @@ public:
         return res;
     }
 
-    int maxProfit(vector<int> &prices) {
+    int maxProfit_2(vector<int> &prices) {
         int res = 0;
         for (int i = 1; i < prices.size(); ++i)
-        {
             if (prices[i] > prices[i-1])
                 res += prices[i] - prices[i-1];
-        }
         return res;
     }
 };
