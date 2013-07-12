b8c8c2c15c94ffb072c3fa2f3aa76c644ba9123d
AnnieKim <anniekim.pku@gmail.com>
2013-07-12 10:06:57 +0800

    Update solution.
    The original one is wrong since it only tries one single path.

--- JumpGame.h
+++ JumpGame.h
@@ -1,6 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Apr 21, 2013
+ Update:     Jul 12, 2013
  Problem:    Jump Game
  Difficulty: Easy
  Source:     http://leetcode.com/onlinejudge#question_55
@@ -12,12 +13,21 @@
  A = [2,3,1,1,4], return true.
  A = [3,2,1,0,4], return false.
 
- Solution: ...
+ Solution: Updated solution: try every reachable index. Thank to Wenxin Xing for kindly feedback:)
  */
 
 class Solution {
 public:
     bool canJump(int A[], int n) {
+        int reachable = 0;
+        for (int i = 0; i <= reachable && reachable < n-1; ++i)
+            reachable = max(reachable, i + A[i]);
+        return reachable >= n-1;
+    }
+
+    // My first solution, which is totally wrong but still able to pass OJ --|
+    // Wrong answer for test cases: {3,4,0,0,1,1} & {2,2,2,3,0,0}
+    bool canJump_Wrong(int A[], int n) {
         int i = 0;
         while (i < n && A[i] > 0)
             i += A[i];
