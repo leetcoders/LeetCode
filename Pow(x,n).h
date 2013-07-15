cfec58ce20101569932e6f8e0dd02e8ba831a394
Annie <anniekim.pku@gmail.com>
2013-07-15 12:12:59 +0800

    Refactor.

--- Pow(x,n).h
+++ Pow(x,n).h
@@ -1,6 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Apr 6, 2013
+ Update:     Jul 15, 2013
  Problem:    Pow(x, n)
  Difficulty: easy
  Source:     http://leetcode.com/onlinejudge#question_50
@@ -13,21 +14,20 @@
 class Solution {
 public:
     double pow(double x, int n) {
-        if (x == 1)
-            return 1;
         if (x < 0)
-            return (n%2 == 0) ? pow(-x, n) : -pow(-x, n);
-        if (n == 0)
-            return 1;
-        if (n == 1)
+            return (n % 2 == 0) ? pow(-x, n) : -pow(-x, n);
+        if (x == 1)
             return x;
         if (n < 0)
-            return 1.0/pow(x, -n);
-        
-        double a = pow(x, n/2);
-        if (n%2 ==0)
-            return a*a;
+            return 1.0 / pow(x, -n);
+        if (n == 0)
+            return 1.0;
+        if (n == 1)
+            return x;
+        double half = pow(x, n / 2);
+        if (n % 2 == 0)
+            return half * half;
         else
-            return x*a*a;
+            return x * half * half;
     }
 };
