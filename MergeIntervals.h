6c46ef45de6a175f0ffc3af52089e29ed4422bf0
Annie <anniekim.pku@gmail.com>
2013-07-15 15:43:32 +0800

    Refactor: DO NOT need to sort according to Interval::end.

--- MergeIntervals.h
+++ MergeIntervals.h
@@ -1,6 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Jun 7, 2013
+ Update:     Jul 15, 2013
  Problem:    Merge Intervals
  Difficulty: Medium
  Source:     http://leetcode.com/onlinejudge#question_56
@@ -11,14 +12,11 @@
  return [1,6],[8,10],[15,18].
 
  Solution: 1. Sort first.
-           - sort according to increasing sequence of 'start' first;
-           - for the intervals that have equal 'start', sort in decreasing order of 'end'.
-           For example, sort [[0,4],[5,9],[6,9],[9,9],[5,5],[8,11],[9,13]],
-           then we will get [[0,4],[5,9],[5,5],[6,9],[8,11],[9,13],[9,9]].
+              - sort according to increasing sequence of 'start'.
            2. Traverse the 'intervals'.
-           - if 'it' interval is totally enclosed by 'last' interval, push 'last' to result, then move both forward.
-           - else merge 'it' and 'last' (modify 'last'), then move 'it' forward only.
-           At last, DO NOT forget push the last 'last' into result.
+              - if 'it' interval intersects with 'last' interval, merge them (modify 'last').
+              - else push 'last' into final result.
+              - At last, DO NOT forget push the last 'last' into result.
 */
 
 /**
@@ -32,17 +30,13 @@
  */
 bool compare(Interval a, Interval b)
 {
-    if (a.start != b.start)
-        return a.start < b.start;
-    else
-        return a.end > b.end;
+    return a.start < b.start;
 }
 
 class Solution {
 public:
     vector<Interval> merge(vector<Interval> &intervals) {
-        if (intervals.empty()) 
-            return intervals;
+        if (intervals.empty()) return intervals;
         sort(intervals.begin(), intervals.end(), compare);
         vector<Interval> res;
         vector<Interval>::iterator last = intervals.begin(), it = last + 1;
