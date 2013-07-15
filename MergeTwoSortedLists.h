ae63548f98210d2f2c4fbd5a29d1650cc7f0b2c4
Annie <anniekim.pku@gmail.com>
2013-07-15 10:15:11 +0800

    Avoid using stupid 'new' operation. Simplify logic.

--- MergeTwoSortedLists.h
+++ MergeTwoSortedLists.h
@@ -1,6 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Apr 17, 2013
+ Update:     Jul 15, 2013
  Problem:    Merge Two Sorted Lists
  Difficulty: Easy
  Source:     http://leetcode.com/onlinejudge#question_21
@@ -19,34 +20,40 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
 class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        if (!l1 && !l2) return NULL;
         if (!l1) return l2;
         if (!l2) return l1;
-        ListNode *res = NULL, *cur;
+        ListNode *head = NULL, *cur = NULL;
         while (l1 && l2)
         {
-            bool useL1;
-            if (l1->val > l2->val)
-                useL1 = false;
-            else
-                useL1 = true;
-
-            if (!res) {
-                res = new ListNode(useL1 ? l1->val : l2->val);
-                cur = res;
-            } else {
-                cur->next = new ListNode(useL1 ? l1->val : l2->val);
-                cur = cur->next;
+            if (l1->val < l2->val)
+            {
+                if (!head) {
+                    head = l1;
+                    cur = head;
+                } else {
+                    cur->next = l1;
+                    cur = cur->next;
+                }
+                l1 = l1->next;
             }
-
-            l1 = useL1 ? l1->next : l1;
-            l2 = useL1 ? l2 : l2->next;
+            else
+            {
+                if (!head) {
+                    head = l2;
+                    cur = head;
+                } else {
+                    cur->next = l2;
+                    cur = cur->next;
+                }
+                l2 = l2->next;
+            }
         }
         if (l1) cur->next = l1;
         if (l2) cur->next = l2;
-        return res;
+        return head;
     }
 };
