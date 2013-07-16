e5ab20f6881924589af0ab1e3fd17ded294b3cb4
Annie <anniekim.pku@gmail.com>
2013-07-16 21:46:18 +0800

    Let j start from 0 for better understanding.

--- RemoveDuplicatesfromSortedArray.h
+++ RemoveDuplicatesfromSortedArray.h
@@ -1,6 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Apr 10, 2013
+ Update:     Jul 16, 2013
  Problem:    Remove Duplicates from Sorted Array
  Difficulty: Easy
  Source:     http://leetcode.com/onlinejudge#question_26
@@ -11,17 +12,17 @@
  Given input array A = [1,1,2],
  Your function should return length = 2, and A is now [1,2].
 
- Solution: ...
+ Solution: Update 7/16/2013: Let j start from 0 for better understanding.
  */
 
 class Solution {
 public:
     int removeDuplicates(int A[], int n) {
         if (n == 0 || n == 1) return n;
-        int j = 1;
+        int j = 0;
         for (int i = 1; i < n; i++)
-            if (A[i] != A[j-1])
-                A[j++] = A[i];
-        return j;
+            if (A[i] != A[j])
+                A[++j] = A[i];
+        return j + 1;
     }
 };
