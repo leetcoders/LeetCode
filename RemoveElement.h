c199b55ae6b6be7be684061d44e9c8f3f8968388
Annie <anniekim.pku@gmail.com>
2013-07-20 10:56:47 +0800

    Update solution. Use two pointers.

--- RemoveElement.h
+++ RemoveElement.h
@@ -1,6 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Apr 10, 2013
+ Update:     Jul 20, 2013
  Problem:    Remove Element
  Difficulty: Easy
  Source:     http://leetcode.com/onlinejudge#question_27
@@ -8,18 +9,16 @@
  Given an array and a value, remove all instances of that value in place and return the new length.
  The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
- Solution: Move the back elements to the front if the front is equal to 'elem'.
+ Solution: Refactor: Update solution. Use two pointers.
  */
 
 class Solution {
 public:
     int removeElement(int A[], int n, int elem) {
-        int i = 0;
-        int j = n - 1;
-        while (i < j)
-            if (A[i] == elem) A[i] = A[j--];
-            else i++;
-        
-        return A[i] == elem ? i : min(n, i + 1);
+        int i = 0, j = 0;
+        for (int j = 0; j < n; ++j)
+            if (A[j] != elem)
+                A[i++] = A[j];
+        return i;
     }
 };
