aecd4fc1790fcc656f333c03bb1a26641388a278
Annie <anniekim.pku@gmail.com>
2013-07-23 21:53:03 +0800

    Improve formatting.

--- UniquePathsII.h
+++ UniquePathsII.h
@@ -30,9 +30,9 @@ public:
         if (obstacleGrid[0][0] == 1) return 0;
         dp[0][0] = 1;
         for (int i = 1; i < m; i++)
-            dp[i][0] = obstacleGrid[i][0] == 1? 0 : dp[i-1][0];
+            dp[i][0] = obstacleGrid[i][0] == 1 ? 0 : dp[i-1][0];
         for (int j = 1; j < n; j++)
-            dp[0][j] = obstacleGrid[0][j] == 1? 0 : dp[0][j-1];
+            dp[0][j] = obstacleGrid[0][j] == 1 ? 0 : dp[0][j-1];
         
         for (int i = 1; i < m; i++)
             for (int j = 1; j < n; j++)
