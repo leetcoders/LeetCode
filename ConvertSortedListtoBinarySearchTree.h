6d18860c41208ec43caa674435cea5f534733f42
Annie <anniekim.pku@gmail.com>
2013-07-29 10:49:35 +0800

    Keep pre-order solution only.

--- ConvertSortedListtoBinarySearchTree.h
+++ ConvertSortedListtoBinarySearchTree.h
@@ -1,15 +1,14 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Apr 10, 2013
+ Update:     Jul 29, 2013
  Problem:    Convert Sorted List to Binary Search Tree
  Difficulty: Medium
  Source:     http://leetcode.com/onlinejudge#question_109
  Notes:
  Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
 
- Solution: Recursion. 
- 1. In-order. This will take more time than pre-order, since we need to traverse to the middle point first.
- 2. Pre-order. O(n)
+ Solution: Recursion. Pre-order. O(n)
  */
 
 /**
@@ -32,40 +31,30 @@
 class Solution {
 public:
     TreeNode *sortedListToBST(ListNode *head) {
-        if (!head) return NULL;
-        ListNode *cur = head;
-        int len = 1;
-        while (cur = cur->next)
-            len++;
-        
-        return buildBST_Pre(head, len);
+        return sortedListToBSTRe(head, getLength(head));
     }
     
-    // In-order
-    TreeNode *buildBST_In(ListNode *head, int len)
+    TreeNode *sortedListToBSTRe(ListNode *&head, int length)
     {
-        if (len == 0 ) return NULL;
-        if (len == 1) return new TreeNode(head->val);
-        
-        ListNode *cur = head;
-        for (int i = 0; i < len / 2; i++)
-            cur = cur->next;
-        
-        TreeNode *root = new TreeNode(cur->val);
-        root->left = buildBST_In(head, len / 2);
-        root->right = buildBST_In(cur->next, len%2==0 ? (len/2-1) : len/2);
-        return root;
-    }
-    
-    // Pre-order, O(n)
-    TreeNode *buildBST_Pre(ListNode *&head, int len)
-    {
-        if (len == 0 ) return NULL;
-        TreeNode *left = buildBST_Pre(head, len / 2);
+        if (length == 0) return NULL;
+        int mid = length / 2;
+        TreeNode *left = sortedListToBSTRe(head, mid);
         TreeNode *root = new TreeNode(head->val);
+        TreeNode *right = sortedListToBSTRe(head->next, length - mid - 1);
         root->left = left;
+        root->right = right;
         head = head->next;
-        root->right = buildBST_Pre(head, len%2==0 ? (len/2-1) : len/2);
         return root;
+    }
+    
+    int getLength(ListNode *head)
+    {
+        int length = 0;
+        while (head)
+        {
+            length++;
+            head = head->next;
+        }
+        return length;
     }
 };
