cb25c93430f5b6153551af6a931c763838f74c33
AnnieKim <anniekim.pku@gmail.com>
2013-07-30 09:29:00 +0800

    One-dimensional DP instead of two.

--- MinimumPathSum.h
+++ MinimumPathSum.h
@@ -1,6 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Apr 17, 2013
+ Update:     Jul 30, 2013
  Problem:    Minimum Path Sum
  Difficulty: Easy
  Source:     http://leetcode.com/onlinejudge#question_64
@@ -9,27 +10,26 @@
  which minimizes the sum of all numbers along its path.
  Note: You can only move either down or right at any point in time.
 
- Solution: Dynamic Programming.
+ Solution: Dynamic Programming. Space O(N).
  */
 
 class Solution {
 public:
     int minPathSum(vector<vector<int> > &grid) {
-        if (grid.size() == 0) return 0;
-        int row = grid.size();
-        int col = grid[0].size();
-        int dp[row][col];
-
-        dp[0][0] = grid[0][0];
-        for (int j = 1; j < col; ++j)
-            dp[0][j] = dp[0][j-1] + grid[0][j];
-        for (int i = 1; i < row; ++i)
-            dp[i][0] = dp[i-1][0] + grid[i][0];
-
-        for (int i = 1; i < row; ++i)
-            for (int j = 1; j < col; ++j)
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
-
-        return dp[row-1][col-1];
+        if (grid.empty()) return INT_MIN;
+        int M = grid.size(), N = grid[0].size();
+        int dp[N];
+        dp[0] = grid[0][0];
+        for (int i = 1; i < N; ++i)
+            dp[i] = grid[0][i] + dp[i-1];
+        
+        for (int i = 1; i < M; ++i)
+        {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < N; ++j)
+                dp[j] = min(dp[j-1], dp[j]) + grid[i][j];
+        }
+        
+        return dp[N-1];
     }
 };
