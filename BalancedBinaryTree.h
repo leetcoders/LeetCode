23efd06bc5351a062678d0cf2f96ed19608d3f92
AnnieKim <anniekim.pku@gmail.com>
2013-08-04 01:25:51 +0900

    Refactor.

--- BalancedBinaryTree.h
+++ BalancedBinaryTree.h
@@ -1,6 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Apr 10, 2013
+ Update:     Aug 4, 2013
  Problem:    Balanced Binary Tree
  Difficulty: Easy
  Source:     http://leetcode.com/onlinejudge#question_110
@@ -9,7 +10,7 @@
  For this problem, a height-balanced binary tree is defined as a binary tree in which 
  the depth of the two subtrees of every node never differ by more than 1.
 
- Solution: Check height.
+ Solution: DFS.
  */
 
 /**
@@ -24,21 +25,19 @@
 class Solution {
 public:
     bool isBalanced(TreeNode *root) {
-        int height;
+        int height = 0;
         return isBalancedRe(root, height);
     }
     
     bool isBalancedRe(TreeNode *root, int &height){
-        if (!root){
-            height = 0; 
-            return true;
-        }
+        if (!root) return true;
         
-        int leftHeight, rightHeight;
+        int leftHeight = 0, rightHeight = 0;
         if (!isBalancedRe(root->left, leftHeight)) return false;
         if (!isBalancedRe(root->right, rightHeight)) return false;
+        if (abs(leftHeight-rightHeight) > 1) return false;
         
         height = 1 + max(leftHeight, rightHeight);
-        return abs(leftHeight-rightHeight) <= 1;
+        return true;
     }
 };
