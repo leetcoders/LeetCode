ccee29029f0fe2d661c81bc220351593ff6e8660
AnnieKim <anniekim.pku@gmail.com>
2013-08-08 02:54:20 +0900

    Update solution.

--- LongestConsecutiveSequence.h
+++ LongestConsecutiveSequence.h
@@ -1,8 +1,9 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       May 24, 2013
+ Update:     Aug 8, 2013
  Problem:    Longest Consecutive Sequence
- Difficulty: Medium
+ Difficulty: Hard
  Source:     http://leetcode.com/onlinejudge#question_128
  Notes:
  Given an unsorted array of integers, find the length of the longest consecutive 
@@ -12,45 +13,38 @@
  The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
  Your algorithm should run in O(n) complexity.
 
- Solution: Store lower-bound and upper-bound for each element in 'num'. O(n) ?
-           Another solution can be found at https://github.com/anson627/leetcode/blob/master/LongestConsecutiveSequence/LongestConsecutiveSequence.cpp,
-           where it only stores length for each element and it has O(n) time complexity actually.
+ Solution: Update solution. This solution is from Peking2 (http://blog.sina.com.cn/s/blog_b9285de20101iqar.html).
+           This solution is much easier to understand.
 */
 
 class Solution {
 public:
     int longestConsecutive(vector<int> &num) {
-        int N = num.size();
-        unordered_map<int, pair<int, int>> itvl;
-        int res = 1;
-        for (int i = 0; i < N; ++i)
+        set<int> s;
+        for (int i = 0; i < num.size(); ++i)
+            s.insert(num[i]);
+        int res = 0;
+        for (int i = 0; i < num.size() && !s.empty(); ++i)
         {
-            int number = num[i];
-
-            if (itvl.find(number) == itvl.end())
-                itvl[number] = make_pair(number, number);
-            else
-                continue;
-
-            int j = number + 1;
-            int low = number, high = number;
-            while (itvl.find(j) != itvl.end())
+            if (s.find(num[i]) != s.end())
             {
-                high = itvl[j].second;
-                itvl[number].second = high;
-                itvl[j].first = itvl[number].first;
-                j = high + 1;
+                int low = getCount(s, num[i], false);
+                int high = getCount(s, num[i] + 1, true);
+                res = max(res, low + high);
             }
-            j = number - 1;
-            while (itvl.find(j) != itvl.end())
-            {
-                low = itvl[j].first;
-                itvl[number].first = low;
-                itvl[j].second = itvl[number].second;
-                j = low - 1;
-            }
-            res = max(res, high - low + 1);
         }
         return res;
+    }
+
+    int getCount(set<int> &s, int val, bool ascending)
+    {
+        int count = 0;
+        while (s.find(val) != s.end())
+        {
+            s.erase(val);
+            count++;
+            val = ascending ? val + 1 : val - 1;
+        }
+        return count;
     }
 };
