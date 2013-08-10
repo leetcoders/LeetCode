9fc070a04e7c8ab0fefa88c5ded8f54fb4ecd61d
AnnieKim <anniekim.pku@gmail.com>
2013-08-10 18:22:05 +0900

    Simplify the solution.

--- CountandSay.h
+++ CountandSay.h
@@ -1,6 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Apr 8, 2013
+ Update:     Aug 10, 2013
  Problem:    Count and Say
  Difficulty: Easy
  Source:     http://leetcode.com/onlinejudge#question_38
@@ -14,77 +15,28 @@
  Given an integer n, generate the nth sequence.
  Note: The sequence of integers will be represented as a string.
 
- Solution: Count the number of equal characters one by one.
- New in version 2: Use stringstream instead of sprintf for inverting integer into string.
+ Solution: ...
 */
 
 class Solution {
 public:
-    string countAndSay_1(int n) {
-        if (n == 1) return string("1");
-        string old("11");
-        string res;
-        
-        for (int i = 3; i <= n; i++)
+    string countAndSay(int n) {
+        string res = "1";
+        for (int i = 2; i <= n; ++i)
         {
-            int count = 1;
-            char c = old[0];
-            for (int j = 1; j < old.size(); j++)
+            stringstream ss;
+            int k = 0;
+            for (int j = 1; j < res.size(); ++j)
             {
-                if (c == old[j])
+                if (res[j] != res[k])
                 {
-                    count++;
-                }
-                else
-                {
-                    pushString(res, count, c);
-                    c = old[j];
-                    count = 1;
+                    ss << (j - k) << res[k];
+                    k = j;
                 }
             }
-            
-            pushString(res, count, c);
-            old = res;
-            res.clear();
+            ss << (res.size() - k) << res[k];
+            ss >> res;
         }
-        
-        return old;
-    }
-    
-    void pushString(string &s, int count, char c)
-    {
-        char x[2];
-        sprintf(x, "%d\0", count);
-        s += string(x);
-        s.push_back(c);
-    }
-
-    string countAndSay_2(int n) {
-        if (n == 1) return string("1");
-        string res("11");
-        
-        for (int i = 3; i <= n; i++)
-        {
-            int count = 1;
-            char c = res[0];
-            stringstream stream;
-            for (int j = 1; j < res.size(); j++)
-            {
-                if (res[j] == c)
-                {
-                    count++;
-                }
-                else
-                {
-                    stream<<count<<c;
-                    c = res[j];
-                    count = 1;
-                }
-            }
-            stream<<count<<c;
-            res = stream.str();
-        }
-        
         return res;
     }
 };
