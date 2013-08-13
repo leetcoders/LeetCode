ff8d92c5cf89629288faad3e273eb2bab9a2deae
AnnieKim <anniekim.pku@gmail.com>
2013-08-13 21:36:24 +0900

    Delete one solution.

--- RomantoInteger.h
+++ RomantoInteger.h
@@ -8,55 +8,12 @@
  Given a roman numeral, convert it to an integer.
  Input is guaranteed to be within the range from 1 to 3999.
 
- Solution: 1. A direct way (faster)
-           2. <map> (clean)
+ Solution: <map> (clean)
  */
 
 class Solution {
 public:
     int romanToInt(string s) {
-        return romanToInt_1(s);
-    }
-
-    int romanToInt_1(string s) {
-        int N = s.size();
-        int res = 0;
-        for (int i = 0; i < s.size(); ++i)
-        {
-            if (s[i] == 'M')
-                res += 1000;
-            else if (s[i] == 'L')
-                res += 50;
-            else if (s[i] == 'D')
-                res += 500;
-            else if (s[i] == 'V')
-                res += 5;
-            else if (s[i] == 'C')
-            {
-                if (i < N-1 && (s[i+1] == 'M' || s[i+1] == 'D'))
-                    res -= 100;
-                else 
-                    res += 100;
-            }
-            else if (s[i] == 'X')
-            {
-                if (i < N-1 && (s[i+1] == 'C' || s[i+1] == 'L'))
-                    res -= 10;
-                else 
-                    res += 10;
-            }
-            else if (s[i] == 'I')
-            {
-                if (i < N-1 && (s[i+1] == 'X' || s[i+1] == 'V'))
-                    res -= 1;
-                else 
-                    res += 1;
-            }
-        }
-        return res;
-    }
-
-    int romanToInt_2(string s) {
         map<char, int> roman;
         roman['M'] = 1000;
         roman['D'] = 500;
