b202585fb8bf43a4ac4e417ebe3ef6a072d4a7ec
AnnieKim <anniekim.pku@gmail.com>
2013-09-03 00:14:33 +0900

    Add iterative solution with constant space.

--- SwapNodesinPairs.h
+++ SwapNodesinPairs.h
@@ -1,6 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Apr 9, 2013
+ Update:     Sep 3, 2013
  Problem:    Swap Nodes in Pairs
  Difficulty: Medium
  Source:     http://leetcode.com/onlinejudge#question_24
@@ -10,7 +11,8 @@
  Given 1->2->3->4, you should return the list as 2->1->4->3.
  Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
 
- Solution: Use recursion instead of normal solution.
+ Solution: 1. Iterative solution with constant space.
+           2. Recursive solution with O(n) space (for practice).
  */
 
 /**
@@ -24,18 +26,32 @@
 class Solution {
 public:
     ListNode *swapPairs(ListNode *head) {
-        swap(head);
+        return swapPairs_1(head);
+    }
+    
+    ListNode *swapPairs_1(ListNode *head) {
+        if (!head || !head->next) return head;
+        ListNode *first = head, *second = head->next;
+        ListNode *last = NULL;
+        head = head->next;
+        while (first && second)
+        {
+            first->next = second->next;
+            second->next = first;
+            if (last) last->next = second;
+            last = first;
+            first = first->next;
+            second = first ? first->next : NULL;
+        }
         return head;
     }
     
-    void swap(ListNode*& node){
-        if (!node || !node->next) 
-            return;
-        ListNode *next = node->next;
-        ListNode *nextHead= next->next;
-        swap(nextHead);
-        next->next = node;
-        node->next = nextHead;
-        node = next;
+    ListNode *swapPairs_2(ListNode *head) {
+        if (!head || !head->next) return head;
+        ListNode *first = head, *second = head->next;
+        first->next = second->next;
+        second->next = first;
+        first->next = swapPairs(first->next);
+        return second;
     }
 };
