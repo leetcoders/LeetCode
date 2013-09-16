dd9f31a1d267b67b4590019741105974e5247372
AnnieKim <anniekim.pku@gmail.com>
2013-09-16 22:00:08 +0800

    Add iterative solution.

--- Subsets.h
+++ Subsets.h
@@ -1,6 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Apr 24, 2013
+ Update:     Sep 16, 2013
  Problem:    Subsets
  Difficulty: Easy
  Source:     http://leetcode.com/onlinejudge#question_78
@@ -22,33 +23,72 @@
   []
  ]
 
- Solution: Recursion (DFS).
+ Solution: 1. Recursive solution.
+           2. Iterative solution. Contributed by yinlinglin.
  */
 
 class Solution {
 public:
-    vector<vector<int>> res;
-    vector<vector<int>> subsets(vector<int> &S) {
-        res.clear();
-        res.push_back(vector<int>());
+    vector<vector<int> > subsets(vector<int> &S) {
+        return subsets_1(S);
+    }
+    
+    vector<vector<int> > subsets_1(vector<int> &S) {
+        vector<vector<int> > res(1, vector<int>());
         sort(S.begin(), S.end());
-        for (int setSize = 1; setSize <= S.size(); ++setSize) {
-            vector<int> r;
-            subsetsRe(S, r, setSize, 0, -1);
-        }
+        vector<int> set;
+        int N = S.size();
+        for (int i = 1; i <= N; ++i)
+            subsetsRe(S, i, 0, set, res);
         return res;
     }
-
-    void subsetsRe(vector<int> &s, vector<int> &r, int setSize, int deep, int lastIndex)
+    
+    void subsetsRe(vector<int> &S, int size, int start, vector<int> &set, vector<vector<int> > &res)
     {
-        if (deep == setSize) {
-            res.push_back(r);
+        int N = S.size(), M = set.size();
+        if (M == size) {
+            res.push_back(set);
             return;
         }
-        for (int i = lastIndex + 1; i <= s.size() - (setSize - deep); ++i) {
-            r.push_back(s[i]);
-            subsetsRe(s, r, setSize, deep + 1, i);
-            r.pop_back();
+        for (int i = start; i < N && N - i >= size - M; ++i) {
+            set.push_back(S[i]);
+            subsetsRe(S, size, i + 1, set, res);
+            set.pop_back();
         }
+    }
+    
+    vector<vector<int> > subsets_2(vector<int> &S) {
+        vector<vector<int> > res(1, vector<int>());
+        sort(S.begin(), S.end());
+        int N = S.size();
+        for (int size = 1; size <= N; ++size)
+        {
+            int stk[size];
+            stk[0] = 0;
+            int top = 0;
+            while (true)
+            {
+                if (stk[top] == N)
+                {
+                    top--;
+                    if (top < 0) break;
+                    stk[top]++;
+                }
+                else if (top == size - 1) 
+                {
+                    vector<int> set;
+                    for (int i = 0; i < size; ++i)
+                        set.push_back(S[stk[i]]);
+                    res.push_back(set);
+                    stk[top]++;
+                }
+                else
+                {
+                    top++;
+                    stk[top] = stk[top-1] + 1;
+                }
+            }
+        }
+        return res;
     }
 };
