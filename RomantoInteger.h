661df1c7d27f05e749a987825a43cd9e5fd0802c
AnnieKim <anniekim.pku@gmail.com>
2013-09-22 15:51:32 +0800

    Refactor.

--- RomantoInteger.h
+++ RomantoInteger.h
@@ -1,6 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       May 13, 2013
+ Update:     Sep 22, 2013
  Problem:    Roman to Integer
  Difficulty: Easy
  Source:     http://leetcode.com/onlinejudge#question_13
@@ -8,13 +9,13 @@
  Given a roman numeral, convert it to an integer.
  Input is guaranteed to be within the range from 1 to 3999.
 
- Solution: <map> (clean)
+ Solution: use <map> or <unordered_map> (clean)
  */
 
 class Solution {
 public:
     int romanToInt(string s) {
-        map<char, int> roman;
+        unordered_map<char, int> roman;
         roman['M'] = 1000;
         roman['D'] = 500;
         roman['C'] = 100;
@@ -23,15 +24,14 @@ public:
         roman['V'] = 5;
         roman['I'] = 1;
 
-        int res = 0;
-        for (int i = 0; i < s.size() - 1; ++i)
+        int res = 0, N = s.size();
+        for (int i = 0; i < N; ++i)
         {
-            if (roman[s[i]] < roman[s[i+1]])
+            if (i < N-1 && roman[s[i]] < roman[s[i+1]])
                 res -= roman[s[i]];
             else
                 res += roman[s[i]];
         }
-        res += roman[s[s.size()-1]];
         return res;
     }
 };
