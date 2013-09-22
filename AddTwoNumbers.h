5dd4ee9c695557104ebeff6b8bd8ffe0f7d23e6f
AnnieKim <anniekim.pku@gmail.com>
2013-09-22 11:23:12 +0800

    dummy..I still have a long way to go!

--- AddTwoNumbers.h
+++ AddTwoNumbers.h
@@ -1,7 +1,7 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
  Date:       Jan 16, 2013
- Update:     Jul 19, 2013
+ Update:     Sep 22, 2013
  Problem:    Add Two Numbers
  Difficulty: easy
  Source:     http://www.leetcode.com/onlinejudge
@@ -13,7 +13,7 @@
  Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
  Output: 7 -> 0 -> 8
 
- Solution:
+ Solution: dummy head...
 
  */
 
@@ -28,39 +28,25 @@
 class Solution {
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        int carrybit = 0;
-        ListNode *head = NULL, *cur = NULL;
-        while (l1 || l2)
+        ListNode dummy(0), *cur = &dummy;
+        int carry = 0;
+        while (l1 || l2 || carry)
         {
-            int sum = carrybit;
-            if (l1)
-            {
+            int sum = carry;
+            if (l1) {
                 sum += l1->val;
                 l1 = l1->next;
             }
-            if (l2)
-            {
+            if (l2) {
                 sum += l2->val;
                 l2 = l2->next;
             }
-            carrybit = sum / 10;
-            if (!head)
-            {
-                head = new ListNode(sum % 10);
-                cur = head;
-            }
-            else
-            {
-                ListNode *newnode = new ListNode(sum % 10);
-                cur->next = newnode;
-                cur = newnode;
-            }
+            carry = sum >= 10 ? 1 : 0;
+            sum %= 10;
+            ListNode *newNode = new ListNode(sum);
+            cur->next = newNode;
+            cur = newNode;
         }
-        if (carrybit == 1)
-        {
-            ListNode *newnode = new ListNode(1);
-            cur->next = newnode;
-        }
-        return head;
+        return dummy.next;
     }
 };
