2b932bf215c16f80f25e6c19d74789e7c89f2ae2
AnnieKim <anniekim.pku@gmail.com>
2013-09-26 13:53:28 +0800

    Add 'clone graph' and revert '3 sum closest'.

--- 3SumClosest.h
+++ 3SumClosest.h
@@ -11,25 +11,26 @@
  For example, given array S = {-1 2 1 -4}, and target = 1.
  The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 
- Solution: Similar to 3Sum, only taking O(n^2) time complexity.
+ Solution: Similar to 3Sum, taking O(n^2) time complexity.
  */
 
 class Solution {
 public:
     int threeSumClosest(vector<int> &num, int target) {
         int res = INT_MAX;
+        int N = num.size();
         sort(num.begin(), num.end());
-        for (size_t i = 0; i < num.size(); ++i)
+        for (int i = 0; i < N-2; ++i)
         {
-            int l = i + 1, r = num.size() - 1;
+            int l = i + 1, r = N - 1;
             while (l < r)
             {
-                int sum = num[i] + num[l] + num[r];
-                if (res == INT_MAX || abs(sum - target) < abs(res - target))
-                    res = sum;
-                if (sum == target) return res;
-                else if (sum < target) l++;
+                int threesum = num[i] + num[l] + num[r];
+                if (threesum == target) return target;
+                else if (threesum < target) l++;
                 else r--;
+                if (res == INT_MAX || abs(threesum - target) < abs(res - target))
+                    res = threesum;
             }
         }
         return res;
