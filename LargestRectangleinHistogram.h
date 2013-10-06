887b896db09a16aaa852978ef8d353849209d6cf
AnnieKim <anniekim.pku@gmail.com>
2013-10-06 21:06:19 +0800

    Update stack solution. The new one DOES takes O(n) time.

--- LargestRectangleinHistogram.h
+++ LargestRectangleinHistogram.h
@@ -1,6 +1,6 @@
 /*
  Author:     Annie Kim, anniekim.pku@gmail.com
- Date:       Sep 29, 2013
+ Date:       Oct 6, 2013
  Problem:    Largest Rectangle in Histogram
  Difficulty: Hard
  Source:     http://leetcode.com/onlinejudge#question_84
@@ -11,23 +11,14 @@
  Given height = [2,1,5,6,2,3],
  return 10.
 
- Solution: 
- Solution 1: Only calucate area when reaching local maximum value.
- Solution 2: Keep a non-descending stack. 
-             Example: {2, 1, 2}
-             1. push an additional 0 into the vector {2, 1, 2, 0}.
-             2. {2}: stack = {2}, res = 0.
-             3. {1}: popup 2, res = 2; push 1 instead, stack = {1}; push 1, stack = {1, 1}.
-             4. {2}: stack = {1, 1, 2}, res = 0.
-             5. {0}: popup 2, res = 2; 
-                     popup 1, res = 2; 
-                     popup 1, res = 3.
- */
+ Solution: 1. Only calucate area when reaching local maximum value.
+           2. Keep a non-descending stack. O(n).
+*/
 
 class Solution {
 public:
     int largestRectangleArea(vector<int> &height) {
-        return largestRectangleArea_1(height);
+        return largestRectangleArea_2(height);
     }
     
     int largestRectangleArea_1(vector<int> &height) {
@@ -45,19 +36,18 @@ public:
     }
     
     int largestRectangleArea_2(vector<int> &height) {
-        stack<int> stk;
         height.push_back(0);
-        int res = 0, N = height.size();
-        for (int i = 0; i < N; ++i) {
-            int count = 0;
-            while (!stk.empty() && stk.top() > height[i]) {
-                count++;
-                res = max(res, count * stk.top());
-                stk.pop();
+        int res = 0, i = 0, N = height.size();
+        stack<int> stk;
+        while (i < N) 
+        {
+            if (stk.empty() || height[stk.top()] <= height[i])
+                stk.push(i++);
+            else {
+                int index = stk.top(); stk.pop();
+                int width = stk.empty() ? i : i - stk.top() - 1;
+                res = max(res, width * height[index]);
             }
-            while (count--)
-                stk.push(height[i]);
-            stk.push(height[i]);
         }
         return res;
     }
