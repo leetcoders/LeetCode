130dd01545d66b80e70ee0ed477d1a73f47a96a1
AnnieKim <anniekim.pku@gmail.com>
2014-03-28 12:14:20 +0900

    Fix a bug #14 reported by imley.
    In such case: s1 = "b", s2 = "acca", s3 = "abcba", it would return true.

--- InterleavingString.h
+++ InterleavingString.h
@@ -28,11 +28,12 @@ public:
         int M = s1.size(), N = s2.size(), K = s3.size();
         if (M + N != K) return false;
         
-        int dp[N+1][M+1];
+        bool dp[N+1][M+1];
+        dp[0][0] = true;
         for (int i = 1; i <= N; ++i)
-            dp[i][0] = s2[i-1] == s3[i-1];
+            dp[i][0] = dp[i-1][0] && s2[i-1] == s3[i-1];
         for (int j = 1; j <= M; ++j)
-            dp[0][j] = s1[j-1] == s3[j-1];
+            dp[0][j] = dp[0][j-1] && s1[j-1] == s3[j-1];
 
         for (int i = 1; i <= N; ++i)
             for (int j = 1; j <= M; ++j)
