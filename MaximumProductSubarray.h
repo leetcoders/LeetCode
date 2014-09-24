3fb8dee13684dc9a087efc115de4ad0340e1d863
applewjg <wangjingui1988@163.com>
2014-09-24 22:46:50 +0800

    new file: MaximumProductSubarray.h

---
+++ MaximumProductSubarray.h
@@ -0,0 +1,40 @@
+/*
+ Author:     Applewjg, higuige@gmail.com
+ Date:       Sep 24, 2014
+ Update:     Sep 24, 2014
+ Problem:    Maximum Product Subarray
+ Difficulty: Medium
+ Source:     https://oj.leetcode.com/problems/maximum-product-subarray/
+ Notes:
+ Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+ For example, given the array [2,3,-2,4],
+ the contiguous subarray [2,3] has the largest product = 6.
+*/
+class Solution {
+public:
+    int maxProduct(int A[], int n) {
+        if(n==0) return 0;
+        if(n==1) return A[0];
+        int minVal = 0;
+        int product = 1;
+        int res = A[0];
+        for(int i=0;i<n;i++){
+            product = product*A[i];
+            if(product<0){
+                if(minVal==0) minVal=product;
+                else {
+                    res = max(res,product/minVal);
+                    minVal=max(minVal,product);
+                }
+            }else if(product==0){
+                res = max(res,product);
+                product = 1;
+                minVal=0;
+            }else{
+                res = max(res,product);
+            }
+        }
+        return res;
+    }
+};
