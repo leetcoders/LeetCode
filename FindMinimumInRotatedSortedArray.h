a1f9a8493349c20dc91e87213c81b9f6ae693fd2
applewjg <wangjingui1988@163.com>
2014-10-22 23:58:41 +0800

    FindMinimumInRotatedSortedArray.h

---
+++ FindMinimumInRotatedSortedArray.h
@@ -0,0 +1,37 @@
+/*
+ Author:     King, higuige@gmail.com
+ Date:       Oct 22, 2014
+ Problem:    Find Minimum in Rotated Sorted Array
+ Difficulty: Medium
+ Source:     https://oj.leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+ Notes:
+ Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+ (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+ Find the minimum element.
+
+ You may assume no duplicate exists in the array.
+*/
+class Solution {
+public:
+    int findMin(vector<int> &num) {
+        if(num.empty()) return 0;
+        int size = num.size();
+        int left = 0;
+        int right = size - 1;
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            if (num[mid] > num[left]) {
+                if (num[left] < num[right]) {
+                    right = mid - 1;
+                } else {
+                    left = mid;
+                }
+            } else {
+                if (right == mid) left = mid;
+                else right = mid;
+            }
+        }
+        return num[left];
+    }
+};
