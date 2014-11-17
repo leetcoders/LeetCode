6e0b115d40201487e61273e49767ca180f9f9691
applewjg <wangjingui1988@163.com>
2014-11-17 20:15:09 +0800

    update

--- SingleNumberII.h
+++ SingleNumberII.h
@@ -11,12 +11,28 @@
  Your algorithm should have a linear runtime complexity. Could you implement it 
  without using extra memory?
 
- Solution: Count the number of each bit.
+ Solution: 1. Count the number of each bit.
+        2. We can improve this based on the previous solution using three bitmask variables.
+        3. An excellent answer by @ranmocy in LeetCode Discuss:
+        https://oj.leetcode.com/discuss/857/constant-space-solution?show=2542#a2542
 */
 
 class Solution {
 public:
     int singleNumber(int A[], int n) {
+        int count[32]={0};
+        int res = 0;
+        for (int i = 0; i < 32; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if ((A[j]>>i)&1) {
+                    ++count[i];
+                }
+            }
+            res|= ((count[i]%3)<<i);
+        }
+        return res;
+    }
+    int singleNumber_2(int A[], int n) {
         int twice = 0;
         int once = 0;
         for (int i = 0; i < n; ++i) {
@@ -27,5 +43,16 @@ public:
             once  = once & notthree;
         }
         return once;
+    }
+    int singleNumber_3(int A[], int n) {
+        if (A == NULL) return 0;
+        int x0 = ~0, x1 = 0, x2 = 0, t;
+        for (int i = 0; i < n; ++i) {
+            t = x2;
+            x2 = (x1 & A[i]) | (x2 & ~A[i]);
+            x1 = (x0 & A[i]) | (x1 & ~A[i]);
+            x0 = (t & A[i]) | (x0 & ~A[i]);
+        }
+        return x1;
     }
 };
