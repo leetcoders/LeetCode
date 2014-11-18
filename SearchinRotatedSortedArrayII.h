37761a132e646cf53c77b265387f914bef9cdf27
applewjg <wangjingui1988@163.com>
2014-11-18 13:13:48 +0800

    update

--- SearchinRotatedSortedArrayII.h
+++ SearchinRotatedSortedArrayII.h
@@ -1,6 +1,7 @@
 /*
- Author:     Annie Kim, anniekim.pku@gmail.com
+ Author:     Annie Kim, anniekim.pku@gmail.com : King, higuige@gmail.com
  Date:       Apr 8, 2013
+ Update:     Nov 18, 2014
  Problem:    Search in Rotated Sorted Array II
  Difficulty: Medium
  Source:     http://leetcode.com/onlinejudge#question_81
@@ -21,6 +22,34 @@ public:
         for (int i = 0; i < n; i++)
             if (A[i] == target)
                 return true;
+        return false;
+    }
+    bool search_2(int A[], int n, int target) {
+        if (A == NULL) return -1;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) /2;
+            if (A[mid] == target) return true;
+            if (A[left] < A[mid]) {
+                if (A[left] <= target && target < A[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (A[left] > A[mid]) {
+               if (A[mid] < target && target <= A[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                } 
+            } else {
+                if (A[mid] == A[right]) {
+                    ++left, --right;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
         return false;
     }
 };
