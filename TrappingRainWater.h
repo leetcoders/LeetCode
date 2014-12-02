a41e5eb1484a3c2698a3bfacffcc38fb49e51ba8
applewjg <wangjingui1988@163.com>
2014-12-02 20:55:03 +0800

    new solution

--- TrappingRainWater.h
+++ TrappingRainWater.h
@@ -11,7 +11,8 @@
  For example, 
  Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 
- Solution: Find left bound and right bound for each element. O(n).
+ Solution: 1. Find left bound and right bound for each element. O(n).
+           2. more space efficiency. Time: O(n), Space: O(1);
 */
 
 class Solution {
@@ -30,6 +31,26 @@ public:
         int res = 0;
         for (int i = 1; i < n; ++i) {
             res += min(maxLeft[i], maxRight[i]) - A[i];
+        }
+        return res;
+    }
+    int trap_2(int A[], int n) {
+        if (A == NULL || n <= 2) return 0;
+        int left= 1;
+        int right = n - 2;
+        int maxLeft = A[0];
+        int maxRight = A[n - 1];
+        int res = 0;
+        while (left <= right) {
+            if (maxLeft <= maxRight) {
+                res += max(0, maxLeft - A[left]);
+                maxLeft = max(maxLeft, A[left]);
+                ++left;
+            } else {
+                res += max(0, maxRight - A[right]);
+                maxRight = max(maxRight, A[right]);
+                --right;
+            }
         }
         return res;
     }
