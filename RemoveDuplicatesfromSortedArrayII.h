ae7f6ce1334b0903773f0a037b971fb35e64d0f0
applewjg <wangjingui1988@163.com>
2014-12-12 22:53:49 +0800

    update

--- RemoveDuplicatesfromSortedArrayII.h
+++ RemoveDuplicatesfromSortedArrayII.h
@@ -21,7 +21,7 @@ public:
         if (n <= 2) return n;
         int j = 2;
         for (int i = 2; i < n; ++i)
-            if (A[i] != A[j-1] || A[i] != A[j-2])
+            if (A[i] != A[j-2])
                 A[j++] = A[i];
         return j;
     }
