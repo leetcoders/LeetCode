e23781819cb127e2c5eba022b1a6cf2e86b47d9a
applewjg <wangjingui1988@163.com>
2014-12-13 19:56:15 +0800

    new solution

--- LongestSubstringWithoutRepeatingCharacters.h
+++ LongestSubstringWithoutRepeatingCharacters.h
@@ -1,21 +1,22 @@
 /*
- Author:     Annie Kim, anniekim.pku@gmail.com
+ Author:     Annie Kim, anniekim.pku@gmail.com, King, wangjingui@outlook.com
  Date:       Apr 16, 2013
- Update:     Aug 19, 2013
+ Update:     Dec 12, 2014
  Problem:    Longest Substring Without Repeating Characters
  Difficulty: Medium
- Source:     http://leetcode.com/onlinejudge#question_3
+ Source:     https://oj.leetcode.com/problems/longest-substring-without-repeating-characters/
  Notes:
  Given a string, find the length of the longest substring without repeating characters. 
  For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. 
  For "bbbbb" the longest substring is "b", with the length of 1.
 
- Solution: Pay attention when moving the 'start' pointer forward.
+ Solution: 1. Pay attention when moving the 'start' pointer forward.
+           2. More space, but maybe faster.
  */
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
+    int lengthOfLongestSubstring_1(string s) {
         bool exist[256];
         memset(exist, false, sizeof(exist));
         int res = 0;
@@ -27,6 +28,21 @@ public:
             else
                 exist[s[start++]] = false;
             res = max(end - start, res);
+        }
+        return res;
+    }
+    int lengthOfLongestSubstring_2(string s) {
+        int len =  s.length();
+        if(len <= 1) return len;
+        vector<int> hash(256,-1);
+        hash[s[0]]=0;
+        int start = 0, cur = 0, res = 1;
+        while(++cur < len){
+            if(hash[s[cur]]>=start){
+                start = hash[s[cur]]+1;
+            }
+            res = max (res, cur - start + 1);
+            hash[s[cur]] = cur;
         }
         return res;
     }
