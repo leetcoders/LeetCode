5f623ae37d2f9dcef4cec46695cb7549d39ba3a7
applewjg <wangjingui1988@163.com>
2014-12-22 16:42:36 +0800

    update
    Change-Id: I407efd03943a30f00fb7f09a3a1897f1eb5ff701

--- IntegertoRoman.h
+++ IntegertoRoman.h
@@ -17,7 +17,7 @@ public:
                                {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
                                {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
                                {"", "M", "MM", "MMM"}};
-    string intToRoman(int num) {
+    string intToRoman_1(int num) {
         string res;
         int i = 3;
         while (num > 0)
@@ -26,6 +26,18 @@ public:
             res += rom[i][num / divisor];
             num %= divisor;
             i--;
+        }
+        return res;
+    }
+    string intToRoman_2(int num) {
+        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        string numerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        string res;
+        for (int i = 0; i < 13; ++i) {
+            while (num >= values[i]) {
+                num -= values[i];
+                res += numerals[i];
+            }
         }
         return res;
     }
